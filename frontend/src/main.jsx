215b46d3db1e8cb5e169863330d68c58afe0d0c9
Preetabh Awasthi <vishubbkup@gmail.com>
2026-01-01 00:00:13 +0530

    #

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
-import ThemeProvider from "./context/ThemeContext";
+import ThemeProvider from "./Context/ThemeContext";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
